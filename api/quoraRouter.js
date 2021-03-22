f12e0f4ead10b92452178d066e7686b73f4a9088
TcardLab <tcardlab@gmail.com>
2021-03-22 15:59:39 -0400

    router tests

--- api/quoraRouter.js
+++ api/quoraRouter.js
@@ -14,11 +14,11 @@ const routes = Object.entries({
 })
 
 
-exports.quoraRouter = async (fullURL, feedItem) => {
+exports.quoraRouter = async (fullURL, feedItem, test=false) => {
   for ( let [type, pattern] of routes ) {
     if ( routeMatch(fullURL, pattern) ) {
-      console.log(`${type}`)
-      return routeParser[type](fullURL, feedItem)
+      //console.log(`${type}`)
+      return test ? type : routeParser[type](fullURL, feedItem) 
     }
   }
   console.log('failure route:', fullURL)
@@ -26,18 +26,30 @@ exports.quoraRouter = async (fullURL, feedItem) => {
 }
 
 /***   Tests   ***/
-//url = 'https://www.quora.com/Could-we-ever-create-infectious-vaccines/answer/Jennifer-Hu-1' // answer
-//url = 'https://www.quora.com/q/bioscience-club' // tribe
-//url = 'https://www.quora.com/profile/Jeffrey-Brender' // user
-//url = 'https://www.quora.com/unanswered/What-did-the-structure-of-DNA-s-double-helix-suggest-about-DNA-s-properties' // unanswered
-//url = 'https://www.quora.com/Where-can-I-find-good-developers/' //question
-//url = 'https://www.quora.com/q/coronaviruswatch/SARS-CoV-2-reactive-T-cells-in-healthy-donors-and-patients-with-COVID-19' // tribeItem
-
+const urls = Object.entries({
+  answer: 'https://www.quora.com/Could-we-ever-create-infectious-vaccines/answer/Jennifer-Hu-1', // answer
+  tribe: 'https://www.quora.com/q/bioscience-club', // tribe
+  user: 'https://www.quora.com/profile/Jeffrey-Brender', // user
+  unanswered: 'https://www.quora.com/unanswered/What-did-the-structure-of-DNA-s-double-helix-suggest-about-DNA-s-properties', // unanswered
+  question: 'https://www.quora.com/Where-can-I-find-good-developers/', //question
+  tribeItem: 'https://www.quora.com/q/coronaviruswatch/SARS-CoV-2-reactive-T-cells-in-healthy-donors-and-patients-with-COVID-19' // tribeItem
+})
 
 /*** TESTS ***/
 const tests = {}
 tests.default = async function () {
-  return await console.log('hi')
+  return await console.log('run')
 }
+
+const genTest = (type, url) => async () => {
+  return await exports.quoraRouter(url, null, true).then(res=>{
+    console.log(`${type}: `, res===type)
+  })
+}
+
+for([type, url] of urls) {
+  tests[type] = genTest(type, url)
+}
+
 const { test } = require('../utils/test')
 test(module)(tests)
